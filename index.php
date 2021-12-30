a0dcaa63f1fa1e5d47f772033af69763e19b59e5
yosephbernandus <yosephbernandus@gmail.com>
2021-12-30 21:42:48 +0700

    mrtg graph

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Monitoring Sitaro</title>
+	
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/sitaro.css">
+	<link rel="stylesheet" type="text/css" href="assets/fancy/jquery.fancybox.css" media="screen" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Tangerine">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Eczar|Roboto" rel="stylesheet">
+  <script type="text/javascript" src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+  <script type="text/javascript">
+    tinymce.init({
+            selector: "textarea"
+            });
+  </script>
+  <style type="text/css">
+  	#title {
+      font-family: 'Tangerine', serif;
+      font-size: 62px;
+      text-shadow: 4px 4px 4px #aaa;
+    }
+
+    body {
+      font-family: 'Eczar', serif;
+    }
+
+    /*.a{
+    	 display: none;
+		}*/
+  </style>	
+
+</head>
+<center>
+<div id="title">Sitaro</div>
+</center>
+<div class="container">
+	<div class="row">
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17050t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17050t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Papusungan - Tagulandang 2 (17050)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.50/" target="_blank"> Papusungan</a>
+              		<a href="http://192.168.17.51/" target="_blank">Tagulandang 2</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17052t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17052t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Tagulandang 2 - Buhias (17052)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.52/" target="_blank"> Tagulandang 2</a>
+              		<a href="http://192.168.17.53/" target="_blank">Buhias</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17054t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17054t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Buhias - Pusunge (17054)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.55/" target="_blank"> Buhias</a>
+              		<a href="http://192.168.17.54/" target="_blank">Pusunge</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17026t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17026t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Pusunge - Lenganeng 1 (17026)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.26/" target="_blank"> Pusunge</a>
+              		<a href="http://192.168.17.27/" target="_blank">Lenganeng 1</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17020t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17020t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Lenganeng 1 - Makalehi (17020)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.20/" target="_blank"> Lenganeng 1</a>
+              		<a href="http://192.168.17.21/" target="_blank">Makalehi</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17040t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17040t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Makalehi - Ondong (17040)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="" target="_blank"> Makalehi</a>
+              		<a href="http://192.168.17.41/" target="_blank">Ondong</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17018t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17018t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Makalehi - Gahusa (17018)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="" target="_blank"> Makalehi</a>
+              		<a href="http://192.168.17.19/" target="_blank">Gahusa</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17030t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17030t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Makalehi - Sitaro-Ondong Pemda (17030)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="" target="_blank"> Makalehi</a>
+              		<a href="http://192.168.17.31/" target="_blank">Sitaro-Ondong</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17010t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17010t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Tagulandang - Makalehi (17010)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.10/" target="_blank">Tagulandang</a>
+              		<a href="http://192.168.17.11/" target="_blank">Makalehi</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17008t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17008t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Tagulandang - Manado Tua (17008)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.8/" target="_blank"> Tagulandang</a>
+              		<a href="http://192.168.17.9/" target="_blank">Manado Tua</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17006t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17006t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Biz Hotel - Manado Tua (17006)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.6/" target="_blank"> Bizhotel</a>
+              		<a href="http://192.168.17.7/" target="_blank">Manado Tua</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17028t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17028t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Lengganeng 1 - Lengganeng 2 (17028)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.28/" target="_blank"> Lengganeng 1</a>
+              		<a href="http://192.168.17.29/" target="_blank"> Lengganeng 2</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17028t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17028t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Lengganeng 2 - Musi (17022)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.22/" target="_blank">Lengganeng 2</a>
+              		<a href="http://192.168.17.23/" target="_blank">Musi</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17024t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17024t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Musi - Melongoane (17024)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.24/" target="_blank">Musi</a>
+              		<a href="http://192.168.17.25/" target="_blank">Melongoane</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17056t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17056t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Pusunge - Kolongan 1 (17056)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="http://192.168.17.56/" target="_blank">Pusunge</a>
+              		<a href="http://192.168.17.57/" target="_blank">Kolongan 1</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17060t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img src="http://manage.manado.net:81/mrtg/17060t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Kolongan 1 - Kantor Bupati Talaud (17060)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="" target="_blank"> Kolongan 1</a>
+              		<a href="http://192.168.17.61/" target="_blank">Kantor Bupati Talaud</a>
+            	</div>
+        	</div>
+    	</div>
+
+    	<div class="col s6">
+        	<div class="card a">
+            	<div class="card-image large">
+              		<a class="various fancybox.iframe tooltipped" href="http://manage.manado.net:81/mrtg/17058t.html" data-delay="50" data-tooltip="Click to view all MRTG" data-position="right"><img class="responsive-image" src="http://manage.manado.net:81/mrtg/17058t-day.png"></a>
+            	</div>
+            	<div class="card-content">
+              		<p>Kolongan 1 - Melongoane (17058)</p>
+            	</div>
+            	<div class="card-action">
+              		<a href="" target="_blank"> Kolongan 1</a>
+              		<a href="http://192.168.17.59/" target="_blank">Melongoane</a>
+            	</div>
+        	</div>
+    	</div>
+	</div>
+</div>
+
+<table>
+                    <tr>
+                        <td>Nama</td>
+                            <td>:</td>
+                        <td><input  type="text" name="Nama"></td>
+                    </tr>
+                    <tr>
+                        <td>Website</td>
+                             <td>:</td>
+                        <td><input  type="text" name="Website"></td>
+                    </tr>
+                    <tr>
+                       <td>Komentar</td>
+                             <td>:</td>
+                        <td><textarea name="Komentar"></textarea></td>
+                    </tr>
+                </table>
+
+</body>
+  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+  <script type="text/javascript" src="assets/fancy/jquery.fancybox.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+  <script src="assets/js/viewportchecker.js"></script> 
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('.tooltipped').tooltip({delay: 50});
+
+      // $(window).scroll(function(){
+      //  if ($(this).scrollTop()>0) {
+      //    $('.a').fadeIn();
+      //  }
+      //  else
+      //  {
+      //    $('.a').fadeOut();
+      //  }
+      // });
+
+      jQuery('.a').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated bounceInLeft', // Class to add to the elements when they are visible
+        offset: 100    
+      });
+
+      $('.fancybox-media')
+      .attr('rel', 'media-gallery')
+      .fancybox({
+        openEffect : 'none',
+        closeEffect : 'none',
+        prevEffect : 'none',
+        nextEffect : 'none',
+
+        arrows : false,
+        helpers : {
+          media : {},
+          buttons : {}
+        }
+      });
+
+      $(".various").fancybox({
+        maxWidth  : 800,
+        maxHeight : 600,
+        fitToView : false,
+        width   : '80%',
+        height    : '80%',
+        autoSize  : false,
+        closeClick  : false,
+        openEffect  : 'none',
+        closeEffect : 'none'
+      });
+    });
+  </script>
+</html>
